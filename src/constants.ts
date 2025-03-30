f63aa4c988b225e28c6e5b4cbe19f0db49bf9559
Josh <joshdwells@gmail.com>
2025-03-30 18:32:18 -0400

    local backend url for bonztm only

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // src/constants.ts
-export const API_URL = "https://backend.nmz-optimizer.xyz";
+export const API_URL = "http://nms-optimizer-service-service:8080/";
 // export const API_URL = "http://127.0.0.1:5000";
